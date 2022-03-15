9e1c0e99422aef9522a2c62bcd725e3ff690df39
swapnendu-karmakar <ktskarmakar@gmail.com>
2022-03-15 22:29:22 +0530

    Initial commit

---
+++ themes/gruv.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#ebdbb2";
+static const char col_gray3[]       = "#ebdbb2";
+static const char col_gray4[]       = "#282828";
+static const char col_gray5[]       = "#d65d0e";
+static const char col_cyan[]        = "#8ec07c";
---
+++ themes/tomorrow_night_bright.h
@@ -0,0 +1,8 @@
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#c397d8";
+static const char col_gray3[]       = "#eaeaea";
+static const char col_gray4[]       = "#2a2a2a";
+static const char col_gray5[]       = "#d54e53";
+static const char col_cyan[]        = "#b9ca4a";
+
+
